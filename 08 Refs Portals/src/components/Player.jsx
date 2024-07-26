f757417b629f286f8d53e8f0185db6a2a70d87b4
Kinolog76 <vladpasha12@gmail.com>
2024-07-26 12:58:48 +0300

    Add 08 section

--- 08 Refs Portals/src/components/Player.jsx
+++ 08 Refs Portals/src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  let playerName = useRef();
+  const [userName, setUserName] = useState(null);
+
+  function handleClick() {
+    setUserName(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {userName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
